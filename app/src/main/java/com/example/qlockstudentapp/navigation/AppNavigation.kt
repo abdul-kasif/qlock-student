ea9f62d0181d508dfd5adcaf1b325cb974f21c33
abdul-kasif <abdulkasif.ra@gmail.com>
2025-09-20 01:12:57 +0530

    feat(navigation): Set SplashScreen as start destination and pass NavHostController to DashboardScreen for logout

--- app/src/main/java/com/example/qlockstudentapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/qlockstudentapp/navigation/AppNavigation.kt
@@ -1,19 +1,24 @@
 // navigation/AppNavigation.kt
 package com.example.qlockstudentapp.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.qlockstudentapp.ui.screens.auth.EmailOtpScreen
-import com.example.qlockstudentapp.ui.screens.profile.ProfileSetupScreen
 import com.example.qlockstudentapp.ui.screens.dashboard.DashboardScreen
+import com.example.qlockstudentapp.ui.screens.profile.ProfileSetupScreen
+import com.example.qlockstudentapp.ui.screens.splash.SplashScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AppNavigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "email_otp") {
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("splash") { SplashScreen(navController) }
         composable("email_otp") { EmailOtpScreen(navController) }
         composable("profile_setup") { ProfileSetupScreen(navController) }
-        composable("dashboard") { DashboardScreen(navController) }
+        composable("dashboard") { DashboardScreen(navController) } // Pass navController for logout
     }
 }
