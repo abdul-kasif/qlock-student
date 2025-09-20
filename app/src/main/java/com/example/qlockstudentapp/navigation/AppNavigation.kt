64cc39657d37f99afe4df0feb68581920a14109d
abdul-kasif <abdulkasif.ra@gmail.com>
2025-09-20 18:25:38 +0530

    feat(ui): Create LockdownScreen placeholder with test info and submit button and add LockdownScreen route with parameters to navigation graph

--- app/src/main/java/com/example/qlockstudentapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/qlockstudentapp/navigation/AppNavigation.kt
@@ -2,6 +2,7 @@
 package com.example.qlockstudentapp.navigation
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
@@ -9,8 +10,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.qlockstudentapp.ui.screens.auth.EmailOtpScreen
 import com.example.qlockstudentapp.ui.screens.dashboard.DashboardScreen
+import com.example.qlockstudentapp.ui.screens.lockdown.LockdownScreen
 import com.example.qlockstudentapp.ui.screens.profile.ProfileSetupScreen
 import com.example.qlockstudentapp.ui.screens.splash.SplashScreen
+import java.net.URLDecoder
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -20,5 +23,17 @@ fun AppNavigation(navController: NavHostController) {
         composable("email_otp") { EmailOtpScreen(navController) }
         composable("profile_setup") { ProfileSetupScreen(navController) }
         composable("dashboard") { DashboardScreen(navController) } // Pass navController for logout
+        composable("lockdown/{sessionId}/{title}/{googleFormUrl}/{testDurationMinutes}") { backStackEntry ->
+            val sessionId = backStackEntry.arguments?.getString("sessionId")?.toLong() ?: 0L
+            val title = backStackEntry.arguments?.getString("title") ?: "Test"
+            val encodedUrl = backStackEntry.arguments?.getString("googleFormUrl") ?: ""
+            val googleFormUrl = try {
+                URLDecoder.decode(encodedUrl, "UTF-8")
+            } catch (e: Exception) {
+                Log.e("AppNavigation", "Error decoding URL: ${e.message}")
+            }
+            val testDurationMinutes = backStackEntry.arguments?.getString("testDurationMinutes")?.toInt() ?: 0
+            LockdownScreen(navController, sessionId, title, googleFormUrl, testDurationMinutes)
+        }
     }
 }
