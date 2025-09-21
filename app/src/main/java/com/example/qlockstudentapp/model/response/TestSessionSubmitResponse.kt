b40d0af3e0fc9c4a9ed4843b8160c92cd53abcea
abdul-kasif <abdulkasif.ra@gmail.com>
2025-09-21 20:42:14 +0530

    feat(api): Add POST /test_sessions/submit endpoint with request/response models

---
+++ app/src/main/java/com/example/qlockstudentapp/model/response/TestSessionSubmitResponse.kt
@@ -0,0 +1,7 @@
+// model/response/TestSessionSubmitResponse.kt
+package com.example.qlockstudentapp.model.response
+
+data class TestSessionSubmitResponse(
+    val message: String,
+    val submitted_at: String
+)
