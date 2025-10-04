8596536fc78b973afd1c32bd356ef1d99eeec4b7
abdul-kasif <abdulkasif.ra@gmail.com>
2025-10-04 13:42:21 +0530

    feat(MainActivity): Add @RequiresApi annotation
    The `onCreate` method in `MainActivity.kt` now includes the `@RequiresApi(Build.VERSION_CODES.O)` annotation.

--- app/src/main/java/com/example/qlockstudentapp/MainActivity.kt
+++ app/src/main/java/com/example/qlockstudentapp/MainActivity.kt
@@ -1,9 +1,11 @@
 // MainActivity.kt
 package com.example.qlockstudentapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,6 +15,7 @@ import com.example.qlockstudentapp.navigation.AppNavigation
 import com.example.qlockstudentapp.ui.theme.QLockStudentAppTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
