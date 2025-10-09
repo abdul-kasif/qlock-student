748e03f685a5d3bc9ed270bda4de0bdca4744db6
abdul-kasif <abdulkasif.ra@gmail.com>
2025-10-09 21:26:52 +0530

    chore(network): Update local machine IP address
    This commit updates the hardcoded IP address for the local development server from `192.168.31.46` to `192.168.31.176`.
    
    The changes are applied in:
    - `ApiClient.kt` for the base URL.
    - `network_security_config.xml` to permit cleartext traffic for local testing.

--- app/src/main/java/com/example/campqstudent/api/ApiClient.kt
+++ app/src/main/java/com/example/campqstudent/api/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 //    private const val BASE_URL = "http://10.0.2.2:3000/" // Emulator localhost
-    private const val BASE_URL = "http://192.168.31.46:3000/"
+    private const val BASE_URL = "http://192.168.31.176:3000/"
 
 
     private var apiService: ApiService? = null
